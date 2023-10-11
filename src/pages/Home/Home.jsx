42b21d429767eb92bca2dc877ea07f62cbd4d09d
Matheus Loubach <34719454+Matheus-Loubach@users.noreply.github.com>
2023-10-11 03:28:15 -0300

    Feat: CSS Responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <h1 className="text-[35px] font-bold">Matheus Affonso</h1>
           <p className="text-gray-400 text-[12px] font-bold">Rio de Janeiro, Brasil</p>
         </div>
-        <nav className="w-[80%] max-sm:w-[90%] text-start mt-[10px] border-b-[1px] pb-[20px] border-gray-700 text-[1.1em] max-sm:font-bold">
+        <nav className="w-[80%] max-sm:w-[85%] text-start mt-[10px] border-b-[1px] pb-[20px] border-gray-700 text-[1.1em] max-sm:font-bold">
           <p className="text-gray-200">
             Como desenvolvedor web, minha missão é forjar experiências digitais que harmonizem com maestria
             uma estética elegante e uma funcionalidade excepcional. Minha paixão consiste em transformar
